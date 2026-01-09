7662414ff426f9780399abedcf82b416dd1a58e4
mathis <mathiisprv@gmail.com>
2026-01-09 08:56:50 +0100

    fix: change repo links to new ones

--- src/views/About.vue
+++ src/views/About.vue
@@ -118,27 +118,39 @@
         <p class="modal-subtitle">Select which Sono project you'd like to view on GitHub</p>
 
         <div class="repo-links">
-          <a href="https://github.com/mathiiiiiis/SonoWeb" target="_blank" class="repo-link">
+          <a href="https://github.com/appsono/sono-site" target="_blank" class="repo-link">
             <div class="repo-icon">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z" fill="currentColor"/>
               </svg>
             </div>
             <div class="repo-info">
-              <div class="repo-name">SonoWeb</div>
+              <div class="repo-name">sono-site</div>
               <div class="repo-desc">Official Website for Sono</div>
             </div>
           </a>
 
-          <a href="https://github.com/mathiiiiiis/SonoAccounts" target="_blank" class="repo-link">
+          <a href="https://github.com/appsono/sono-web" target="_blank" class="repo-link">
             <div class="repo-icon">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z" fill="currentColor"/>
               </svg>
             </div>
             <div class="repo-info">
-              <div class="repo-name">SonoAccounts</div>
+              <div class="repo-name">sono-web</div>
               <div class="repo-desc">Web Application for Sono</div>
+            </div>
+          </a>
+
+          <a href="https://github.com/appsono/sono-api" target="_blank" class="repo-link">
+            <div class="repo-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z" fill="currentColor"/>
+              </svg>
+            </div>
+            <div class="repo-info">
+              <div class="repo-name">sono-api</div>
+              <div class="repo-desc">API for Sono</div>
             </div>
           </a>
 
@@ -149,7 +161,7 @@
               </svg>
             </div>
             <div class="repo-info">
-              <div class="repo-name">SonoFlutterApp</div>
+              <div class="repo-name">sono-mobile</div>
               <div class="repo-desc">Mobile app (Private repository)</div>
             </div>
           </div>
